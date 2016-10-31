9581aaf64020f18df6dd36a516a33318feeda0d9
Carlos Avilan <carlosa@ludopia.net>
2016-10-31 11:43:55 -0400

    Add missing features

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ var fs      = require('fs');
 var path    = require('path');
 var templater = require('./templater.js');
 
+// TODO: change commander, maybe even improve it
 program
   .version('0.0.1')
-  .option('-n, --new', 'New project')
+  .option('-n, --new', 'New app')
   .option('-m, --module [name]', 'Module name', 'app')
-  .option('-M, --new-module <name>', 'New module') // TODO
+  .option('-M, --new-module <name>', 'New module')
   .option('-s, --service <name>', 'New Service')
-  .option('-f, --factory <name>', 'New Factory')
-  .option('-fi, --filter <name>', 'New Filter') // TODO
+  .option('-F, --factory <name>', 'New Factory')
+  .option('-f, --filter <name>', 'New Filter')
   .option('-c, --controller <name>', 'New Controller')
-  .option('-d, --directive <name>', 'New Directive') // TODO
+  .option('-d, --directive <name>', 'New Directive')
   .option('-r, --route <state> [templateUrl] [url] [controller]', 'New Route') // TODO
-  .option('-rf, --route-file <name>', 'New Route file') // TODO
-  .option('-cp, --component <name>', 'New Component') // TODO
-  .option('-p, --partial <name>', 'New Partial') // TODO
+  .option('-R, --route-file <name>', 'New Route file')
+  .option('-C, --component <name>', 'New Component')
+  .option('-p, --provider <name>', 'New Provider')
   .parse(process.argv);
 
-// New project
+// New app
 if (program.new) {
-  console.log('Creating New Project');
+  console.log('Creating new app');
   console.log('Creating files structure');
 
   if (!fs.existsSync('scripts')){
@@ -60,14 +61,24 @@ if (program.new) {
     console.log('Created gulpfile.js');
   });
 
-  fs.writeFile(path.join('app','app.module.js'), templater.module('app', 'app'), function (err) {
+  fs.writeFile(path.join('app','app.module.js'), templater.module('app'), function (err) {
     if (err) return console.log(err);
     console.log('Created app.module.js');
   });
 
-  fs.writeFile(path.join('app','app.config.js'), templater.config('app', 'app'), function (err) {
+  fs.writeFile(path.join('app','app.config.js'), templater.generic('config', 'app', 'config'), function (err) {
     if (err) return console.log(err);
     console.log('Created app.config.js');
+  });
+}
+
+// New Provider
+if (program.provider) {
+  console.log('Adding new provider ');
+  var fname = program.provider + '.provider.js';
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.provider, program.module, 'provider'), function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + fname);
   });
 }
 
@@ -75,7 +86,7 @@ if (program.new) {
 if (program.service) {
   console.log('Adding new service ');
   var fname = program.service + '.service.js';
-  fs.writeFile(path.join(program.module, fname), templater.service(program.service, program.module), function (err) {
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.service, program.module, 'service'), function (err) {
     if (err) return console.log(err);
     console.log('Created ' + fname);
   });
@@ -85,39 +96,89 @@ if (program.service) {
 if (program.factory) {
   console.log('Adding new factory ');
   var fname = program.factory + '.factory.js';
-  fs.writeFile(path.join(program.module, fname), templater.factory(program.factory, program.module), function (err) {
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.factory, program.module, 'factory'), function (err) {
     if (err) return console.log(err);
     console.log('Created ' + fname);
   });
 }
 
-// TODO: New Filter
+// New Filter
+if (program.filter) {
+  console.log('Adding new filter ');
+  var fname = program.filter + '.filter.js';
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.filter, program.module, 'filter'), function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + fname);
+  });
+}
 
 // New Controller
 if (program.controller) {
   console.log('Adding new controller ');
   var fname = program.controller + '.controller.js';
-  fs.writeFile(path.join(program.module, fname), templater.controller(program.controller, program.module), function (err) {
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.controller, program.module, 'controller'), function (err) {
     if (err) return console.log(err);
     console.log('Created ' + fname);
   });
 }
 
-// TODO: New Directive
+// New Directive
+if (program.directive) {
+  console.log('Adding new directive ');
+  var fname = program.directive + '.directive.js';
+  var tname = program.directive + '.directive.html';
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.directive, program.module, 'directive'), function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + fname);
+  });
 
-// TODO: New Component
+  fs.writeFile(path.join(program.module, tname), '', function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + tname);
+  });
+}
 
-// TODO: New Partial
+// New Component
+if (program.component) {
+  console.log('Adding new component ');
+  var fname = program.component + '.component.js';
+  var tname = program.directive + '.component.html';
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.component, program.module, 'component'), function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + fname);
+  });
+
+  fs.writeFile(path.join(program.module, tname), '', function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + tname);
+  });
+}
+
+// New Routes file
+if (program.routeFile) {
+  console.log('Adding new route file ');
+  var fname = program.routeFile + '.route.js';
+  fs.writeFile(path.join(program.module, fname), templater.generic(program.routeFile, program.module, 'route'), function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + fname);
+  });
+}
+
+// New Module
+if (program.newModule) {
+  console.log('Adding new module ');
+  var fname = program.newModule + '.module.js';
+  fs.writeFile(path.join(program.module, fname), templater.module(program.newModule), function (err) {
+    if (err) return console.log(err);
+    console.log('Created ' + fname);
+  });
+}
 
 // TODO: New Route
 if (program.route) {
   // console.log('state: ', program.route);
   // console.log(program.args);
 }
-
-// TODO: New Routes file
-
-// TODO: New Module
 
 // TODO: group in folders by prefix
 // TODO: check if exists first and ask if user wants to overwrite
